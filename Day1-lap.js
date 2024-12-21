73d46bcd430fc75c3d36fd308c9328aff0badb00
MahmoudNasrZ <Mahmoudn.zalabany256@gmail.com>
2024-12-21 15:57:32 +0200

    first commit v3

--- Day1-lap.js
+++ Day1-lap.js
@@ -1,5 +1,4 @@
 // function Sort(a, b, c) {
-//   var d;
 //   if (a < b) {
 //     [a, b] = [b, a];
 //   }
@@ -9,68 +8,63 @@
 //   if (a < b) {
 //     [a, b] = [b, a];
 //   }
-//   //   alert(`${a},${b},${c}`),
+//   //   alert(`${a},${b},${c}`), alert(a + "," + b + "," + c);
 //   return console.log(a + "," + b + "," + c);
 // }
 
-// Sort(-7, 5, -4);
+// Sort(-500, -21, 20);
 
-// // function FizzBuzz(num) {
-// //   while (num <= 100) {
-// //     if (num % 3 === 0 && num % 5 === 0) {
-// //       console.log("FizzBuzz");
-// //     } else if (num % 3 === 0) {
-// //       console.log("Fizz");
-// //     } else if (num % 5 === 0) {
-// //       console.log("Buzz");
-// //     } else {
-// //       console.log(num);
-// //     }
-// //     num++;
-// //   }
-// // }
+// function FizzBuzz(num) {
+//   while (num <= 100) {
+//     if (num % 3 === 0 && num % 5 === 0) {
+//       console.log("FizzBuzz");
+//     } else if (num % 3 === 0) {
+//       console.log("Fizz");
+//     } else if (num % 5 === 0) {
+//       console.log("Buzz");
+//     } else {
+//       console.log(num);
+//     }
+//     num++;
+//   }
+// }
 
-// // FizzBuzz(1);
-// // function FizzBuzz(num) {
-// //   var sum = 0; // Initialize sum
-// //   var i = 0;
+// FizzBuzz(1);
+// function FizzBuzz(num) {
+//   var sum = 0; // Initialize sum
+//   var i = 0;
 
-// //   while (i <= num) {
-// //     if (i % 3 === 0 && i % 5 === 0) {
-// //       sum += i;
-// //     }
-// //     i++;
-// //   }
+//   while (i <= num) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       sum += i;
+//     }
+//     i++;
+//   }
 
-// //   return sum;
-// // }
+//   return sum;
+// }
 
-// // console.log(FizzBuzz(1000));
+// console.log(FizzBuzz(1000));
 
-// // function RandomNum(Userguess) {
-// //   num = 3;
-// //   num = Math.floor(Math.random() * 11);
-// //   flag = 0;
-// //   while (true) {
-// //     if (Userguess == num) {
-// //       console.log("Good");
-// //       break;
-// //     } else if (flag < 3) {
-// //       console.log("Bad Answer");
-// //       var Userguess = prompt(
-// //         "wrong guess, want to try again? Guess Between 0 - 10"
-// //       );
-// //     } else {
-// //       alert("Try Again Another Time");
-// //       break;
-// //     }
-// //     flag++;
-// //   }
-// // }
-// // var guess = prompt("Please enter your a Guess Between 0 - 10");
-// // RandomNum(guess);
-// // function happyNum(){
-// // if(num >0)
-
-// // }
-// // happyNum()
+function RandomNum(Userguess) {
+  num = 3;
+  //   num = Math.floor(Math.random() * 11);
+  flag = 0;
+  while (true) {
+    if (parseInt(Userguess) == num) {
+      console.log("Good");
+      break;
+    } else if (flag < 3) {
+      console.log("Bad Answer");
+      var Userguess = prompt(
+        "wrong guess, want to try again? Guess Between 0 - 10"
+      );
+    } else {
+      alert("Try Again Another Time");
+      break;
+    }
+    flag++;
+  }
+}
+var guess = prompt("Please enter your a Guess Between 0 - 10");
+RandomNum(guess);
